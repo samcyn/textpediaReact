ab430fe9806f5d6605cadbfe19736f51e5777677
samcyn <samsoniyanda@outlook.com>
2018-10-08 13:33:32 +0100

    set up a fresh react project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
-
-import Header from './Components/layout/Header';
-import Navbar from './Components/layout/Navbar';
-import { BrowserRouter } from 'react-router-dom';
-
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <section className="bg shadow"></section>
-        <BrowserRouter>
-        <div>
-          <Header />
-          <Navbar />
-        </div>
-        </BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
--- src/utils/index.js
+++
@@ -1,2 +0,0 @@
-export const endpoint_url = 'https://textpedia-api.herokuapp.com/'
-export const isValidEmail = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
