d327a33a5bad0215af8fc6c8a295163e616a546b
samcyn <samsoniyanda@outlook.com>
2018-10-09 03:28:33 +0100

    implements form submission

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,12 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: ``
+  baseURL: "https://textpedia-api.herokuapp.com/",
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true,
+    'Access-Control-Allow-Headers': 'Content-Type, Accept, Access-Control-Allow-Origin'
+  }
 });
--- src/components/pages/Register/Register.js
+++ src/components/pages/Register/Register.js
@@ -1,35 +1,98 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment } from "react";
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
+
+// A X I O S
+import API from "../../../api/api";
+
 import "./Register.css";
 import Button from "../../common/Button/Button";
 
-const Register = () => (
-  <Fragment>
-    <span>TRY TEXTPEDIA</span>
-    <h1>SIGN UP</h1>
+class Register extends Component {
+  state = {
+    phone: '',
+    email: ''
+  }
 
-    <form className="form">
-      <div className="field">
-        <label className="label has-text-primary">Phone Number</label>
-        <div className="control">
-          <input className="input" type="number" placeholder="Phone Number" />
-        </div>
-        {/* <p class="help">This is a help text</p> */}
-      </div>
-      <div className="field">
-        <label className="label has-text-primary">Email Address</label>
-        <div className="control">
-          <input className="input" type="email" placeholder="Email Address" />
-        </div>
-        {/* <p class="help">This is a help text</p> */}
-      </div>
-      <div className="field ">
-        <div className="control">
-          {/* reuseable component */}
-          <Button className="is-primary">Sign Up</Button>
-        </div>
-      </div>
-    </form>
-  </Fragment>
-);
+  handleChangle = (e) => {
+    const {value, name } = e.target;
+    this.setState({
+      [name] : value
+    });
+  }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    //submit form
+    const { phone, email } = this.state;
+    let data = {
+      data: {
+        phoneNumber: phone,
+        email
+      }
+    };
+    //post form data
+    API.post('submit', data)
+    .then(result => {
+      this.props.history.push({
+        pathname: '/confirmation',
+        state: { result: result.data.jwt }
+      });
+    })
+    .catch(err => {
+      if (err.response.status === 409) {
+        this.setState({ error: err.response.data })
+      } else {
+        this.setState({ error: 'Ops... error occured' })
+      }
+      this.setState({ show: false });
+    });
+  }
+
+  render() {
+    const { email, phone } = this.state;
+    return (
+      <Fragment>
+        <span>TRY TEXTPEDIA</span>
+        <h1>SIGN UP</h1>
+
+        <form className="form" onSubmit = { this.handleFormSubmit }>
+          <div className="field">
+            <label className="label has-text-primary">Phone Number</label>
+            <div className="control">
+              <PhoneInput 
+                className="input"
+                name="phone"
+                placeholder="Enter phone number"
+                value={ phone}
+                onChange={ phone => this.setState({ phone })}/>
+            </div>
+            {/* <p class="help">This is a help text</p> */}
+          </div>
+          <div className="field">
+            <label className="label has-text-primary">Email Address</label>
+            <div className="control">
+              <input
+                className="input"
+                type="email"
+                name="email"
+                placeholder="Email Address"
+                value = { email }
+                onChange = { this.handleChangle }
+              />
+            </div>
+            {/* <p class="help">This is a help text</p> */}
+          </div>
+          <div className="field ">
+            <div className="control">
+              {/* reuseable component */}
+              <Button className="is-primary ">Sign Up</Button>
+            </div>
+          </div>
+        </form>
+      </Fragment>
+    );
+  }
+}
 
 export default Register;
